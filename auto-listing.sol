987591766e3885b1d17d538f7ebc190cc70d1aae
Dexaran <dexaran820@gmail.com>
2024-01-24 19:38:02 +0400

    Update auto-listing.sol

--- auto-listing.sol
+++ auto-listing.sol
@@ -4,39 +4,34 @@ contract Autolisting
 {
     string public name = "DEX223 autolisting contract";
 
-    uint256 num_batches = 0;
-    uint8   batch_size  = 10;
-    mapping (uint256 => address[]) public pairs;
-
-    // Adds pair contract addresses in batches.
-    // `pairs[0]` contains 10 pair addresses
-    // `pairs[1]` contains another 10 addresses
-    // and so on ...
-    function add_pairs(address[] calldata _pairs) public
+    uint256 public num_pairs;
+    uint256 public last_update = block.timestamp;
+    mapping (uint256 => address) public pairs;
+    
+    function add_pairs(address[] memory _pairs) public /* onlyOwner */
     {
-        // Just some logic of adding addresses in batches.
-        uint256 _position = pairs[num_batches].length;
+        uint256 _position = num_pairs;
+        num_pairs += _pairs.length;
 
-        for (uint i = _pairs.length; i > 0; i--)
+        for (uint i = 0; i < _pairs.length; i++)
         {
-            pairs[num_batches].push( _pairs[i] );
+            pairs[_position] = _pairs[i];
             _position++;
-            if (_position == batch_size)
-            {
-                num_batches++;
-                _position = 0;
-            }
         }
+
+        //pairs[num_pairs] = _pairs[0];
+        last_update = block.timestamp;
     }
 
-    // The UI is supposed to call this function to obtain `batch_size` number of pair addresses
-    // starting from `batch_id` = 0 until `batch_id` becomes equal to `num_batches`
-    // upon reaching `num_batches` the UI considers that all pairs stored in the contract are red.
-    //
-    // The UI expects to get `batch_size` number of addresses per call. Therefore we are
-    // reducing the number of API calls by `batch_size` times.
-    function UI_query_pairs(uint256 batch_id) public view returns (address[] memory)
+    function get_pairs(uint256 _start, uint256 _end) public view returns (address[] memory)
     {
-        return pairs[batch_id];
+        address[] memory _pairs = new address[](_end - _start);
+        uint256 _array_index = 0;
+        for (uint256 i = _start; i < _end; i++)
+        {
+            _pairs[_array_index] = pairs[i];
+            _array_index++;
+        }
+        return _pairs;
     }
 }
